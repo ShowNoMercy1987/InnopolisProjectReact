31eeb43bf4844f63d2bfb6384c52ec81893994f1
Dmitriy Ovchinnikov <dlcerberos@gmail.com>
2023-03-16 02:09:57 +0300

    hash router

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,21 +1,18 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
 import PageIndex from "./components/PageIndex/PageIndex";
-import PageProduct from './components/PageProduct/PageProduct';
-import PageNotFound from './components/PageNotFound/PageNotFound';
-
-
-
+import PageProduct from "./components/PageProduct/PageProduct";
+import PageNotFound from "./components/PageNotFound/PageNotFound";
 
 function App() {
   return (
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<PageIndex />} />
-      <Route path="/product" element={<PageProduct />} />
-      <Route path="*" element={<PageNotFound />} />
-    </Routes>
-  </BrowserRouter>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<PageIndex />} />
+        <Route path="/product" element={<PageProduct />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </HashRouter>
   );
 }
 
